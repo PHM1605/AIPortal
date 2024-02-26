be3ef9bb69faef80ba562452e947530cf45baf25
RTDevOps <RTDevOps@dmspro.vn>
2024-02-26 17:26:52 +0700

    requirements

--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ app.use(cors({
   credentials: true
 }));
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PWD,
   database: process.env.DB_NAME
 });
