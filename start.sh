d0d69bce7c77ab49689fab0ebae10a3c2107999a
Hung Dang <hung.dang980@gmail.com>
2024-02-27 01:04:39 +0700

    fix code path

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ ls -la /app
 ls -la /app/server
 ls -la /app/client
 ls -la /client/dist
-python apiServer/main.py &
-cd server && node index.js 
+cd /app/apiServer && python main.py &
+cd /app/server && node index.js 
